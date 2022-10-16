4883b161d8d0c3e24199dad4d8f20a28600a933c
Faisal <faisal.faisi.fm@gmail.com>
2022-10-16 16:02:39 +0500

    Http Request and Listview Builder Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../drawer.dart';
-import '../my_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,13 +12,23 @@ class HomePage extends StatefulWidget {
 
 // stateless widget is that which dont change its state like text on scrreen
 class _HomePageState extends State<HomePage> {
-  var myName = "Change Your Name";
-  final TextEditingController _nameController = TextEditingController();
+  // var myName = "Change Your Name";
+  // final TextEditingController _nameController = TextEditingController();
+
+  var url = "http://jsonplaceholder.typicode.com/photos";
+  var res_data;
 
   @override
   void initState() {
     //? to initialize the state
     super.initState();
+    fetchData(); //? this is custom function
+  }
+
+  fetchData() async {
+    var res = await http.get(Uri.parse(url));
+    res_data = jsonDecode(res.body);
+    setState(() {}); //? when ever there is change in UI then call setState
   }
 
   @override
@@ -35,24 +45,28 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Faisi App"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: SingleChildScrollView(
-            child: MyCardWidget(
-                myName: myName,
-                nameController: _nameController //? imported class
-                ),
-          ),
-        ),
-      ),
+      body: res_data != null
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(res_data[index]["title"]),
+                  subtitle: Text(
+                      "ID : ${res_data[index]["id"]}"), //?  string interpolation
+                  leading: Image.network(res_data[index]["url"]),
+                );
+              },
+              itemCount: res_data.length,
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer:
           const MyDrawer(), //? MyDrawer is a class which is returning the drawer widget created by ourself
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //? this empty implementation of function (){} means void function
-          myName = _nameController.text;
-          setState(() {}); //* this is to reflect the change on UI
+          //  myName = _nameController.text;
+          //  setState(() {}); //* this is to reflect the change on UI
         },
         child: const Icon(Icons.send),
       ),
